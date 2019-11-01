8c42a10d5a1dd13282b811e179e24f648ac2bd70
ronCohen2 <ronc665@gmail.com>
2019-11-01 21:31:34 +0200

    finish push/pop method

--- LinkedList.js
+++ LinkedList.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(value, node) {
+  constructor(value) {
     this.value = value;
     this.node = null;
   }
@@ -10,13 +10,42 @@ class LinkedList {
     this.tail = null;
     this.length = 0;
   }
-  push = () => {};
-  pops = () => {};
-  shift = () => {};
-  unshift = () => {};
-  get = () => {};
-  set = () => {};
-  insert = () => {};
-  remove = () => {};
-  reverse = () => {};
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+  if(!this.head) return undefined
+  
 }
+
+let list = new LinkedList();
+list.push("1");
+list.push("2");
+list.pop();
+console.log(list);
