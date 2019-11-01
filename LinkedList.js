23743d767181b9480a280e42cbc784c502aa333b
ronCohen2 <ronc665@gmail.com>
2019-11-01 11:54:32 +0200

    initial Commit

---
+++ LinkedList.js
@@ -0,0 +1,22 @@
+class Node {
+  constructor(value, node) {
+    this.value = value;
+    this.node = null;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push = () => {};
+  pops = () => {};
+  shift = () => {};
+  unshift = () => {};
+  get = () => {};
+  set = () => {};
+  insert = () => {};
+  remove = () => {};
+  reverse = () => {};
+}
